01a5147d4514816c448aef9d9690bf635283fe45
clarenochieng <98944975+clarenochieng@users.noreply.github.com>
2022-04-29 11:57:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import yfinance as yf
-#import plotly.graph_objects as go
-from datetime import date, datetime
+import datetime
 
-today = date.today().strftime("%Y-%M-%D")
-default_start_date = today - datetime.timedelta(days=700)
+today = datetime.date.today()
+default_start_date = today - datetime.timedelta(days=365)
 start_date = st.sidebar.date_input("Select start date: ", default_start_date)
 end_date = st.sidebar.date_input("Select end date: ", today)
 if start_date < end_date:
@@ -13,7 +12,6 @@ if start_date < end_date:
 else:
     st.sidebar.error("End date cannot come before start date.")
 
-
 st.title("Nairobi Forex Corner")
 
 stocks = ("AAPL", "AMD", "AMZN", "FB", "GOOG", "MSFT", "NFLX", "NVDA", "QCOM", "TSLA")
@@ -21,7 +19,7 @@ stock_option = st.sidebar.selectbox("Select stock:", stocks)
 
 stocks_data = yf.Ticker(stock_option)
 
-stocks_dataframe = stocks_data.history(period = "id", start = start_date, end = end_date)
+stocks_dataframe = stocks_data.history(period="id", start=start_date, end=end_date)
 
 st.write("### Close Values")
 st.line_chart(stocks_dataframe["Close"])
