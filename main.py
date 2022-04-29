3bb0df284e94870e396a698f1b5227355c7ab099
clarenochieng <98944975+clarenochieng@users.noreply.github.com>
2022-04-29 10:14:17 +0300

    Create main.py
    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import yfinance as yf
+from datetime import date
+
+START = "2015-01-01"
+END = date.today().strftime("%Y-%M-%D")
+
+st.title("Nairobi Forex Corner")
+
+stocks = ("AAPL", "GOOG")
+stocks_select_box = st.selectbox("Select stock:", stocks)
+
+stocks_data = yf.Ticker(stocks)
+
+stocks_dataframe = stocks_data.history(period = "id", start = START, end = "2021-12-31")
+
+st.write("### Close Values")
+st.line_chart(stocks_dataframe["Close"])
+
+st.write(" *** ")
+
+st.write("### Volume Chart")
+st.line_chart(stocks_dataframe["Volume"])
