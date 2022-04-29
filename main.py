885ef4146b91f347bfa7bd369b50dcf58c39d807
clarenochieng <98944975+clarenochieng@users.noreply.github.com>
2022-04-29 12:02:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ import streamlit as st
 import yfinance as yf
 import datetime
 
+stocks = ("AAPL", "AMD", "AMZN", "FB", "GOOG", "MSFT", "NFLX", "NVDA", "QCOM", "TSLA")
+stock_option = st.sidebar.selectbox("Select stock:", stocks)
+
 today = datetime.date.today()
 default_start_date = today - datetime.timedelta(days=365)
 start_date = st.sidebar.date_input("Select start date: ", default_start_date)
 end_date = st.sidebar.date_input("Select end date: ", today)
-if start_date < end_date:
+if start_date <= end_date:
     st.sidebar.success(f'Start date: {start_date}')
     st.sidebar.success(f'End date: {end_date}')
 else:
     st.sidebar.error("End date cannot come before start date.")
 
 st.title("Nairobi Forex Corner")
-
-stocks = ("AAPL", "AMD", "AMZN", "FB", "GOOG", "MSFT", "NFLX", "NVDA", "QCOM", "TSLA")
-stock_option = st.sidebar.selectbox("Select stock:", stocks)
 
 stocks_data = yf.Ticker(stock_option)
 
