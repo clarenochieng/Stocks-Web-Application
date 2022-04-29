8272fc24ab2d29b408effb15082f2b59e13517ef
clarenochieng <98944975+clarenochieng@users.noreply.github.com>
2022-04-29 11:29:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,24 @@ import yfinance as yf
 #import plotly.graph_objects as go
 from datetime import date
 
-START = "2015-01-01"
-END = date.today().strftime("%Y-%M-%D")
+today = date.today().strftime("%Y-%M-%D")
+start_date = st.sidebar.date_input("Select start date: ", "2020-01-01")
+end_date = st.sidebar.date_input("Select end date: ", today)
+if start_date < end_date:
+    st.sidebar.success(f'Start date: {start_date}')
+    st.sidebar.success(f'End date: {end_date}')
+else:
+    st.sidebar.error("End date cannot come before start date.")
+
 
 st.title("Nairobi Forex Corner")
 
 stocks = ("AAPL", "AMD", "AMZN", "FB", "GOOG", "MSFT", "NFLX", "NVDA", "QCOM", "TSLA")
-stocks_select_box = st.selectbox("Select stock:", stocks)
+stock_option = st.sidebar.selectbox("Select stock:", stocks)
 
-start_date = st.date_input("Select the start date: ", min_value = "2015-01-01", max_value = date.today().strftime("%Y-%M-%D"))
+stocks_data = yf.Ticker(stock_option)
 
-stocks_data = yf.Ticker(stocks_select_box)
-
-stocks_dataframe = stocks_data.history(period = "id", start = start_date, end = "2021-12-31")
+stocks_dataframe = stocks_data.history(period = "id", start = start_date, end = end_date)
 
 st.write("### Close Values")
 st.line_chart(stocks_dataframe["Close"])
