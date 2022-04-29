78cddb5b25aaa52617dcbdce6108bd3a035f2c54
clarenochieng <98944975+clarenochieng@users.noreply.github.com>
2022-04-29 11:52:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import yfinance as yf
 #import plotly.graph_objects as go
 from datetime import date, datetime
 
-default_start_date = datetime.strptime("2020-01-01", "%Y-%m-%d")
-default_start_date = default_start_date.strftime("%Y-%m-%d")
 today = date.today().strftime("%Y-%M-%D")
+default_start_date = today - datetime.timedelta(days=700)
 start_date = st.sidebar.date_input("Select start date: ", default_start_date)
 end_date = st.sidebar.date_input("Select end date: ", today)
 if start_date < end_date:
