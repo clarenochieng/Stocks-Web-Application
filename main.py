b61e001a1ac15349c73a7fec0f1097fad2308db9
clarenochieng <98944975+clarenochieng@users.noreply.github.com>
2022-04-29 10:56:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import yfinance as yf
+import plotly.graph_objects as go
 from datetime import date
 
 START = "2015-01-01"
@@ -10,9 +11,11 @@ st.title("Nairobi Forex Corner")
 stocks = ("AAPL", "AMD", "AMZN", "FB", "GOOG", "MSFT", "NFLX", "NVDA", "QCOM", "TSLA")
 stocks_select_box = st.selectbox("Select stock:", stocks)
 
+start_date = st.date_input("Select the start date: ", min_value = "2015-01-01", max_value = date.today().strftime("%Y-%M-%D"))
+
 stocks_data = yf.Ticker(stocks_select_box)
 
-stocks_dataframe = stocks_data.history(period = "id", start = START, end = "2021-12-31")
+stocks_dataframe = stocks_data.history(period = "id", start = start_date, end = "2021-12-31")
 
 st.write("### Close Values")
 st.line_chart(stocks_dataframe["Close"])
